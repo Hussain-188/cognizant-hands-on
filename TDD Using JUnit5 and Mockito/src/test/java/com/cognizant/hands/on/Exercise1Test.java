f5f48b3331a9db3d52ab3689c7df385b7fc69a96
Hussain <hussain090905@gmail.com>
2026-06-26 22:47:23 +0530

    Exercise 1: Setting Up JUnit

---
+++ TDD Using JUnit5 and Mockito/src/test/java/com/cognizant/hands/on/Exercise1Test.java
@@ -0,0 +1,12 @@
+package com.cognizant.hands.on;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class Exercise1Test {
+
+    @Test
+    public void jUnitIsConfigured() {
+        assertTrue(true);
+    }
+}
